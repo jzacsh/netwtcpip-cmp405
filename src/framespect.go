1c9c69418c5fc9d7dcb24459960f568849cab88e
Jonathan Zacsh <jzacsh@gmail.com>
2017-09-29 01:41:54 -0400

    dead since porting to and finishing github.com/jzacsh/hexint

--- src/framespect.go
+++ src/framespect.go
@@ -78,49 +78,6 @@ func decodeHexStream(input io.Reader) ([]byte, error) {
 	return bytes, nil
 }
 
-func mustIntFromHex(hex byte) int {
-	resp, e := getIntFromHex(hex)
-	if e != nil {
-		panic(e)
-	}
-	return resp
-}
-
-// Given a hex char, returns its corresponding integer: a value in [0,15]
-func getIntFromHex(hex byte) (int, error) {
-	// Char-to-rune table:
-	//
-	// | char    code point
-	// +-------------------
-	// | [0,9] = [48,57]
-	// | [A,F] = [65,70]
-	// | [a,f] = [97,102]
-
-	runePoint := rune(hex)
-	val := getValidHex(runePoint)
-	if val == -1 {
-		return -1, fmt.Errorf("invalid hex, got: '%c'", runePoint)
-	}
-	return val, nil // TODO(zacsh) finish converting to int [0,15]
-}
-
-func isValidHex(rn rune) bool { return getValidHex(rn) != -1 }
-
-func getValidHex(rn rune) int {
-	switch {
-	case (rn >= 48 && rn <= 57): /*[0,9]*/
-		fmt.Fprintf(os.Stderr, "think '%c' is [0,9]\n", rn) // TODO(zacsh) debugging; remove
-	case (rn >= 65 && rn <= 70): /*[A,F]*/
-		fmt.Fprintf(os.Stderr, "think '%c' is [A,F]\n", rn) // TODO(zacsh) debugging; remove
-	case (rn >= 97 && rn <= 102): /*[a,f]*/
-		fmt.Fprintf(os.Stderr, "think '%c' is [a,f]\n", rn) // TODO(zacsh) debugging; remove
-	default:
-		return -1
-	}
-	fmt.Fprintf(os.Stderr, "think '%c' IS hex; returning %d\n", rn, int(rn)) // TODO(zacsh) debugging; remove
-	return int(rn)
-}
-
 func main() {
 	bytes, e := decodeHexStream(os.Stdin)
 	if e != nil {
