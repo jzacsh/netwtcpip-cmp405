46885fb98fd395e3967e6b6041f9b8e605fc5fb7
Jonathan Zacsh <jzacsh@gmail.com>
2017-10-01 17:24:33 -0400

    noop(refactor) share logic for other homeworks

--- hw04.go
+++ hw04.go
@@ -2,91 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"./src/parseip4"
 )
 
-type Octets uint32 // all 4 octets, in their respective bit locations
-
-type OctsList []int // len always 4
-
-func (o OctsList) Pack() Octets {
-	return (Octets(o[0]) << 24) +
-		(Octets(o[1]) << 16) +
-		(Octets(o[2]) << 8) +
-		(Octets(o[3]))
-}
-
-func (ino Octets) List() OctsList {
-	return OctsList{
-		int((0xFF000000 & ino) >> 24),
-		int((0x00FF0000 & ino) >> 16),
-		int((0x0000FF00 & ino) >> 8),
-		int(0x000000FF & ino),
-	}
-}
-
-type Addr struct {
-	ip   OctsList
-	mask OctsList
-}
-
-func (a *Addr) String() string {
-	return fmt.Sprintf(
-		"%d.%d.%d.%d/%d.%d.%d.%d",
-		a.ip[0], a.ip[1], a.ip[2], a.ip[3],
-		a.mask[0], a.mask[1], a.mask[2], a.mask[3])
-}
-
-func (a *Addr) Classful() (OctsList, uint, string) {
-	topOctet := a.ip[0]
-	switch {
-	case topOctet < 128:
-		return OctsList{255, 0, 0, 0}, 8 * 1, "A"
-	case topOctet >= 128 && topOctet < 192:
-		return OctsList{255, 255, 0, 0}, 8 * 2, "B"
-	case topOctet >= 192 && topOctet < 224:
-		return OctsList{255, 255, 255, 0}, 8 * 3, "C"
-	default:
-		panic("expected only classes A,B, or C")
-	}
-}
-
-var hosts = []Addr{
-	{ip: OctsList{9, 201, 195, 84}, mask: OctsList{255, 255, 240, 0}},
-	{ip: OctsList{128, 10, 189, 215}, mask: OctsList{255, 255, 248, 0}},
-	{ip: OctsList{135, 21, 243, 82}, mask: OctsList{255, 255, 224, 0}},
-	{ip: OctsList{75, 149, 205, 61}, mask: OctsList{255, 255, 192, 0}},
-	{ip: OctsList{7, 105, 198, 111}, mask: OctsList{255, 255, 252, 0}},
+var hosts = []parseip4.Addr{
+	{IP: parseip4.NewAddr(9, 201, 195, 84), Mask: parseip4.NewAddr(255, 255, 240, 0)},
+	{IP: parseip4.NewAddr(128, 10, 189, 215), Mask: parseip4.NewAddr(255, 255, 248, 0)},
+	{IP: parseip4.NewAddr(135, 21, 243, 82), Mask: parseip4.NewAddr(255, 255, 224, 0)},
+	{IP: parseip4.NewAddr(75, 149, 205, 61), Mask: parseip4.NewAddr(255, 255, 192, 0)},
+	{IP: parseip4.NewAddr(7, 105, 198, 111), Mask: parseip4.NewAddr(255, 255, 252, 0)},
 
 	// TODO(zacsh) remove this sample from the last slide
-	{ip: OctsList{128, 10, 211, 78}, mask: OctsList{255, 255, 240, 0}},
-}
-
-func countSubnetBits(o Octets) uint {
-	var n uint = 0
-	for i := n; i < 32; i++ {
-		n += 1 & (uint(o) >> i)
-	}
-	return n
-}
-
-func (a *Addr) NetworkIndex() Octets {
-	classMask, cidrOffset, _ := a.Classful()
-	return (a.ip.Pack() & classMask.Pack()) >> (32 - cidrOffset)
-}
-
-func (a *Addr) SubnetIndex() Octets {
-	classMask, cidrOffset, _ := a.Classful()
-	classMaskPck := classMask.Pack()
-
-	uniqSubnetBits := (^classMaskPck) & a.mask.Pack()
-	middleSubnetBits := uniqSubnetBits & a.ip.Pack()
-	totalNetBitsCount := cidrOffset + countSubnetBits(uniqSubnetBits)
-	return middleSubnetBits >> (32 - totalNetBitsCount)
-}
-
-func (a *Addr) HostIndex() Octets {
-	maskPck := a.mask.Pack()
-	return (^maskPck) & a.ip.Pack()
+	{IP: parseip4.NewAddr(128, 10, 211, 78), Mask: parseip4.NewAddr(255, 255, 240, 0)},
 }
 
 func main() {
@@ -96,7 +24,7 @@ func main() {
 
 		fmt.Printf(
 			"  network: %v (class %s masked)\n\t%v\n\tnetwork id:\t%d\n\t subnet id:\t%d\n\t   host id:\t%d\n\n",
-			(addr.ip.Pack() & classMask.Pack()).List(), klass,
+			(addr.IP.Pack() & classMask.Pack()).List(), klass,
 			addr.String(),
 			addr.NetworkIndex(),
 			addr.SubnetIndex(),
---
+++ src/parseip4/parseip4.go
@@ -0,0 +1,106 @@
+// Package parseip4 provides some common functionality for inspecting an IP
+// address, per IP v4, including addresses using CIDR.
+package parseip4
+
+import "fmt"
+
+// Octets represents all 4 octets of an address, in as single unsigned integer.
+// To produce a sensible Octets value, see OctsList.Pack().
+type Octets uint32
+
+// OctsList is a (presumed) len()=4 list of 8-bit unsigned integer values, or
+// octets.
+type OctsList []int // TODO(zacsh) make this a uint8
+
+// Addr represents an IP address and its mask, which may or may not be a
+// default classful addressing mask.
+type Addr struct {
+	IP   OctsList
+	Mask OctsList
+}
+
+// NewAddr builds a proper-length OctsList.
+func NewAddr(highest, second, third, lowest int) OctsList {
+	return OctsList{highest, second, third, lowest}
+}
+
+// Pack produces a single integer with tthe values of OctsList in the
+// appropriate bit location.
+func (o OctsList) Pack() Octets {
+	return (Octets(o[0]) << 24) +
+		(Octets(o[1]) << 16) +
+		(Octets(o[2]) << 8) +
+		(Octets(o[3]))
+}
+
+// OctsList unpacks a Octets value into its component 8-bit valued integers.
+func (ino Octets) List() OctsList {
+	return OctsList{
+		int((0xFF000000 & ino) >> 24),
+		int((0x00FF0000 & ino) >> 16),
+		int((0x0000FF00 & ino) >> 8),
+		int(0x000000FF & ino),
+	}
+}
+
+func (a *Addr) String() string {
+	return fmt.Sprintf(
+		"%d.%d.%d.%d/%d.%d.%d.%d",
+		a.IP[0], a.IP[1], a.IP[2], a.IP[3],
+		a.Mask[0], a.Mask[1], a.Mask[2], a.Mask[3])
+}
+
+// Classful determines the classful-addressing "class" of an IP address and
+// returns three representations of this: a mask, a CIDR-notation offset, and
+// the human-readable class label.
+func (a *Addr) Classful() (OctsList, uint, string) { // TODO(zacsh) make this a `OctsList` address
+	topOctet := a.IP[0]
+	switch {
+	case topOctet < 128:
+		return OctsList{255, 0, 0, 0}, 8 * 1, "A"
+	case topOctet >= 128 && topOctet < 192:
+		return OctsList{255, 255, 0, 0}, 8 * 2, "B"
+	case topOctet >= 192 && topOctet < 224:
+		return OctsList{255, 255, 255, 0}, 8 * 3, "C"
+	default:
+		panic("expected only classes A,B, or C")
+	}
+}
+
+func countSubnetBits(o Octets) uint {
+	var n uint = 0
+	for i := n; i < 32; i++ {
+		n += 1 & (uint(o) >> i)
+	}
+	return n
+}
+
+// NetworkIndex determines what the integer index of an address's network number
+// would be, if it were considered as one of a continuous stream of potential
+// networks within the IP address space.
+func (a *Addr) NetworkIndex() Octets {
+	classMask, cidrOffset, _ := a.Classful()
+	return (a.IP.Pack() & classMask.Pack()) >> (32 - cidrOffset)
+}
+
+// SubnetIndex determines what the integer index of an address's subnet number
+// would be, if it were considered as one of a continuous stream of potential
+// networks within the IP address space.
+func (a *Addr) SubnetIndex() Octets {
+	classMask, cidrOffset, _ := a.Classful()
+	classMaskPck := classMask.Pack()
+
+	uniqSubnetBits := (^classMaskPck) & a.Mask.Pack()
+	middleSubnetBits := uniqSubnetBits & a.IP.Pack()
+	totalNetBitsCount := cidrOffset + countSubnetBits(uniqSubnetBits)
+	return middleSubnetBits >> (32 - totalNetBitsCount)
+}
+
+// HostIndex determines what the integer index of an address's host number
+// would be - after considering any classless subnet apparently being utilized,
+// according to the addresses' mask - if it were considered as one of a
+// continuous stream of potential networks within the IP address space.
+func (a *Addr) HostIndex() Octets {
+	maskPck := a.Mask.Pack()
+	return (^maskPck) & a.IP.Pack()
+}
