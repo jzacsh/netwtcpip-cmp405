542bfcfa1c576ea6d41b21137f606299be20471a
Jonathan Zacsh <jzacsh@gmail.com>
2017-10-11 19:30:34 -0400

    noop(whitespace) fix broken indentation

--- hw06/SendReceive/src/SendReceiveSocket.java
+++ hw06/SendReceive/src/SendReceiveSocket.java
@@ -8,37 +8,37 @@ public class SendReceiveSocket {
 
   public static void receiveMethod() {
 
-  DatagramSocket inSocket = null;
-  byte[] inBuffer = new byte[100];
-  DatagramPacket inPacket = new DatagramPacket(inBuffer, inBuffer.length);
+    DatagramSocket inSocket = null;
+    byte[] inBuffer = new byte[100];
+    DatagramPacket inPacket = new DatagramPacket(inBuffer, inBuffer.length);
 
 
-  try {
-  inSocket = new DatagramSocket(64000, myAddress);
-  } catch (Exception e) {
-  e.printStackTrace();
-  System.exit(-1);
-  }
+    try {
+      inSocket = new DatagramSocket(64000, myAddress);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(-1);
+    }
 
-  do {
-  for ( int i = 0 ; i < inBuffer.length ; i++ ) {
-  inBuffer[i] = ' ';
-  }
+    do {
+      for ( int i = 0 ; i < inBuffer.length ; i++ ) {
+        inBuffer[i] = ' ';
+      }
 
-  try {
-  // this thread will block in the receive call
-  // until a message is received
-  System.out.println("Waiting for input...");
-  inSocket.receive(inPacket);
-  } catch (Exception e) {
-  e.printStackTrace();
-  System.exit(-1);
-  }
+      try {
+        // this thread will block in the receive call
+        // until a message is received
+        System.out.println("Waiting for input...");
+        inSocket.receive(inPacket);
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.exit(-1);
+      }
 
-  String message = new String(inPacket.getData());
-  System.out.println("Received message = " + message);
+      String message = new String(inPacket.getData());
+      System.out.println("Received message = " + message);
 
-  } while(true);
+    } while(true);
   }
 
 
