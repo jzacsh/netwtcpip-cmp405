8ba1f5aee237b92b1aabc3350a397ec9895d9c5a
Jonathan Zacsh <jzacsh@gmail.com>
2017-10-25 20:41:49 -0400

    bugfix: inverted bool usage; probably never called `run()`

--- hw06/src/RunLocked.java
+++ hw06/src/RunLocked.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.locks.Lock;
 
 public class RunLocked {
-  private static void safeRun(Lock locker, Runnable toRun) {
+  public static void safeRun(Lock locker, Runnable toRun) {
     locker.lock();
     try {
       toRun.run();
@@ -10,15 +10,16 @@ public class RunLocked {
     }
   }
 
-  private static boolean safeRunTry(Lock locker, Runnable toRun) {
-    boolean isLocked = false;
+  /** Returns if {@code toRun} was ever executed.  */
+  public static boolean safeRunTry(Lock locker, Runnable toRun) {
+    boolean isAcquired = false;
     try {
-      isLocked = locker.tryLock(MAX_BLOCK_MILLIS, TimeUnit.MILLISECONDS);
+      isAcquired = locker.tryLock(MAX_BLOCK_MILLIS, TimeUnit.MILLISECONDS);
     } catch (InterruptedException e) {
       return false;
     }
-    if (!isLocked) {
-      return isLocked;
+    if (!isAcquired) {
+      return false;
     }
 
     try {
