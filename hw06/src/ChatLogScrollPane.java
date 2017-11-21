jzacsh/netwtcpip-cmp405

commit 33619b048422b2ac204c15d6a3784a0613b08457

broken: 80% of work for lazy-loading refactor
this will eventually make it so *both* homework 6 and 9 modes lazy-load
their validation and blocking work, as part of the window that was
launched. That is -- be it parsing host ip, or requesting, waiting,
parsing username resolutions -- chat windows launch instantly and allow
user to interact once validations complete (giving back control of
launcher-dashboard immediately).

hw06/src/ChatterJFrame.java
@@ -57,7 +57,7 @@ public class ChatterJFrame extends JFrame implements ActionListener {
     txtPanel.add(
         this.addLabeled(
             this.destName = new JTextField(30),
-            this.userResolver == null ? "destination" : "username",
+            this.isUserNameMode() ? "destination" : "username",
             ACTION_DASHBRD_START),
         BorderLayout.LINE_START);
     txtPanel.add(
@@ -78,9 +78,12 @@ public class ChatterJFrame extends JFrame implements ActionListener {
     this.setLocationRelativeTo(null);
     this.setVisible(true);
 
-    this.hist.registerDefaultListener((Remote r) -> this.handleSolicitations(r));
+    this.hist.registerDefaultListener((Remote r) -> this.launchCachedChat(true /*isRecvd*/, r));
+
     this.chats = new HashMap<>();
   }
+
+  private boolean isUserNameMode() { return this.userResolver != null; }
 
   private JPanel addLabeled(
       JTextField subject, String label,
@@ -128,32 +131,27 @@ public class ChatterJFrame extends JFrame implements ActionListener {
       case ACTION_DASHBRD_START:
         this.markDashProcessing();
 
-        ChatStart start = null;
-        final boolean isFailure =
-            !this.isValidFormUsage() ||
-            !(start = ChatStart.parseFrom(this.destName, this.destPort)).isValid();
-
         this.log.printf(
             "validating [dest: '%s', port: '%s']; is failure=%s\n",
-            this.destName.getText(), this.destPort.getText(), isFailure);
-        if (isFailure) {
+            this.destName.getText(), this.destPort.getText(), !this.isValidFormUsage());
+        if (!this.isValidFormUsage()) {
           final String currentID = this.dashBrdID();
           if (this.lastDashFailure != null && currentID.equals(this.lastDashFailure)) {
             this.resetDashBrds();
             return;
           }
 
-          this.dashNoteFailure(
-              currentID,
-              start == null ? "host & port required" : start.getFailReason());
+          this.dashNoteFailure(currentID, "destination & port required");
           this.lastDashFailure = currentID;
           return;
         }
-
-        this.launchCachedChat(start);
         this.log.debugf(
             "starting chat with dest: '%s', port: '%s'...\n",
             this.destName.getText(), this.destPort.getText());
+
+        final Remote target = this.buildUncheckedRemote(this.destName, this.destPort);
+        this.launchCachedChat(false /*isRecvd*/, target);
+
         this.resetDashBrds();
         break;
       default:
@@ -163,61 +161,29 @@ public class ChatterJFrame extends JFrame implements ActionListener {
     }
   }
 
-  private void launchCachedChat(ChatStart s) {
-    final String chatID = s.toString();
-    if (!this.chats.containsKey(chatID)) {
-      this.chats.put(chatID, s.launchChat(this.hist));
+  private Remote buildUncheckedRemote(JTextComponent jDest, JTextComponent jPort) {
+    final String destRaw = jDest.getText().trim();
+    final String portRaw = jPort.getText().trim();
+    return this.isUserNameMode()
+        ? Remote.viaUncheckedName(destRaw, portRaw)
+        : Remote.viaUncheckedAddress(destRaw, portRaw);
+  }
+
+  private void launchCachedChat(boolean isRecvd, final Remote unchecked) {
+    final String launchID = unchecked.toString();
+    if (!this.chats.containsKey(launchID)) {
+      this.chats.put(launchID, this.launchChat(isRecvd, unchecked));
       return;
     }
 
-    MessagingJFrame existing = this.chats.get(chatID);
+    // TODO ensure remove() from this.chats, when window closes
+    MessagingJFrame existing = this.chats.get(launchID);
     existing.setVisible(true);
     existing.setFocusable(true);
     existing.requestFocus();
   }
 
-  private void handleSolicitations(Remote r) {
-    this.launchCachedChat(new ChatStart(r.getHost(), r.getPort()));
-  }
-}
-
-class ChatStart extends Remote {
-  private final String context;
-  private String failure = null;
-
-  public ChatStart(final InetAddress host, int port) {
-    super(host, port);
-    this.context = "recvd";
-  }
-
-  public ChatStart(
-      final String rawHost, final String rawPort,
-      final InetAddress host, int port) {
-    super(host, port);
-    this.context = String.format("started: %s:%s", rawHost, rawPort);
-  }
-
-  private ChatStart(String fail) {
-    this(null /*host*/, -1 /*port*/);
-    this.failure = fail;
-  }
-
-  public boolean isValid() { return this.failure == null; }
-  public String getFailReason() { return this.failure; }
-
-  public static ChatStart parseFrom(JTextComponent jHost, JTextComponent jPort) {
-    final String hostRaw = jHost.getText().trim();
-    final String portRaw = jPort.getText().trim();
-    Remote r = null;
-    try {
-      r = Remote.parseFrom(hostRaw, portRaw);
-    } catch (Throwable e) {
-      return new ChatStart(e.getMessage());
-    }
-    return new ChatStart(hostRaw, portRaw, r.getHost(), r.getPort());
-  }
-
-  public MessagingJFrame launchChat(History hist) {
-    return new MessagingJFrame(this.context, hist, this);
+  private MessagingJFrame launchChat(boolean isRecvd, final Remote target) {
+    return new MessagingJFrame(isRecvd ? "received" : "launched", this.hist, target);
   }
 }

hw06/src/MessagingJFrame.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -49,6 +50,21 @@ public class MessagingJFrame extends JFrame implements ActionListener {
     this.setVisible(true);
     this.composeField.requestFocus();
     this.setLocationRelativeTo(null);
+
+    // TODO(zacsh) implement lazy-loading of remote and meta-log into chat window, and wire results
+    // to handleResolvedRemote()
+    this.composeField.setDisabledTextColor(Color.RED);
+    this.composeField.setText("[loading] please wait...");
+    this.composeField.setEnabled(false);
+    this.sendBtn.setEnabled(false);
+  }
+
+  private void handleResolvedRemote() {
+    if (this.remote.isValid()) {
+      this.composeField.setText("");
+    }
+    this.composeField.setEnabled(this.remote.isValid());
+    this.sendBtn.setEnabled(this.remote.isValid());
   }
 
   public void actionPerformed(ActionEvent e) {

hw06/src/Remote.java
@@ -2,50 +2,100 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Remote {
-  private String userName = null;
+  private static final int UNCHECKED_PORT = -1;
 
+  /** Either username or raw host address. */
+  private String rawDest = null;
+  private String rawPort = null;
+  private boolean isViaUserName = false;
+
+  /** Whether the next two fields are populated and valid. */
+  protected boolean isChecked = false;
   protected InetAddress host = null;
-  protected int port = -1;
+  protected int port = UNCHECKED_PORT;
 
-  public Remote(String userName, final InetAddress host, final int protocolPort) {
-    this.userName = userName;
-    this.host = host;
-    this.port = protocolPort;
-  }
-
-  public Remote(final InetAddress host, int port) {
+  public Remote(final String username, final InetAddress host, int port) {
+    this.isChecked = true;
+    this.isViaUserName = true;
+    this.rawDest = username;
     this.host = host;
     this.port = port;
   }
 
+  public Remote(final InetAddress host, int port) {
+    this.isChecked = true;
+    this.isViaUserName = false;
+    this.host = host;
+    this.port = port;
+  }
+
+  private Remote(String port) {
+    this.rawPort = port;
+    this.isChecked = false;
+  }
+
+  public static Remote viaUncheckedAddress(String host, String port) {
+    Remote unchecked = new Remote(port);
+    unchecked.rawDest = host;
+    unchecked.isViaUserName = false;
+    return unchecked;
+  }
+
+  public static Remote viaUncheckedName(String username, String port) {
+    Remote unchecked = new Remote(port);
+    unchecked.rawDest = username;
+    unchecked.isViaUserName = true;
+    return unchecked;
+  }
+
   /** Whether this is remote host started via just username resolution protocol. */
-  public boolean isViaNameProtocol() { return this.userName != null; }
+  public boolean isViaNameProtocol() { return this.isViaUserName; }
 
   public InetAddress getHost() { return this.host; }
   public int getPort() { return this.port; }
 
   public final String toString() {
-    return String.format("%s:%d", this.host.getHostAddress(), this.port);
+    return String.format(
+        "'%s':'%s':[is%s username service]",
+        this.rawDest, this.rawPort,
+        this.isViaNameProtocol() ? "" : " not");
   }
 
-  public static Remote parseFrom(String hostRaw, String portRaw) throws Exception {
-    InetAddress host = null;
+  public boolean checkAddress() throws Exception {
+    if (this.isViaNameProtocol()) {
+      throw new IllegalStateException("checkAddress() called for a user-name target");
+    }
+
+    if (this.isChecked()) {
+      return this.isValid();
+    }
+    this.isChecked = true;
+
+    InetAddress hostChecked = null;
     try {
-      host = InetAddress.getByName(hostRaw);
+      hostChecked = InetAddress.getByName(this.rawDest);
     } catch (UnknownHostException e) {
-      throw new Error(String.format("could not resolve host '%s'", hostRaw));
+      throw new Error(String.format("could not resolve host '%s'", this.rawDest));
     }
 
-    int port;
+    int portChecked;
     try {
-      port = Integer.parseUnsignedInt(portRaw);
+      portChecked = Integer.parseUnsignedInt(this.rawPort);
     } catch (NumberFormatException e) {
-      throw new Error(String.format("'%s' is not a valid port number", portRaw));
+      throw new Error(String.format("'%s' is not a valid port number", this.rawPort));
     }
 
-    if (!AssertNetwork.isValidPort(port)) {
-      throw new Error(String.format("%d is an invalid port number", port));
+    if (!AssertNetwork.isValidPort(portChecked)) {
+      throw new Error(String.format("%d is an invalid port number", portChecked));
     }
-    return new Remote(host, port);
+
+    this.host = hostChecked;
+    this.port = portChecked;
+    return this.isValid();
+  }
+
+  public boolean isChecked() { return this.isChecked; }
+  public boolean isValid() {
+    return this.isChecked() && this.host != null && this.port != UNCHECKED_PORT;
   }
 }

commit 0e1f42c9e96840b9994775ecce31e16a89e5a0ba

ui bug: rendering chat GUI in username mode
latent-loading GUI changes - started by 33619b0 - were apparently broken
(only noticable in username services mode) due mostly to
ChatLogScrollPane's too-soon call on this History api.

Now ChatLogScrollPane waits until its `Remote` is resolved (in either
mode) before trying to sync logs with hsitory. Aside benefit: the
meta-messaging in this chat log is now easier to follow/understand.

hw06/src/ChatLogScrollPane.java
@@ -9,13 +9,12 @@ import javax.swing.ScrollPaneConstants;
 class ChatLogScrollPane extends JScrollPane implements Runnable {
   private final Logger log = new Logger("ChatLogScrollPane");
   private static final long MAX_BLOCK_MILLIS = 25;
-  private LockedList<Message> src;
+  private LockedList<Message> src = null;
   private int numMessagesSeen;
   private JTextArea textLog;
 
-  public ChatLogScrollPane(int rows, int cols, LockedList<Message> logs) {
+  public ChatLogScrollPane(int rows, int cols) {
     super();
-    this.src = logs;
     this.numMessagesSeen = 0;
 
     this.textLog = new JTextArea(rows, cols);
@@ -29,16 +28,15 @@ class ChatLogScrollPane extends JScrollPane implements Runnable {
     this.textLog.setDisabledTextColor(Color.BLACK);
     this.textLog.setLineWrap(true);
     this.textLog.setWrapStyleWord(true);
-
-    this.renderNewLogs(); // initial rendering of loaded history
   }
+
+  public void syncFrom(LockedList<Message> logs) { this.src = logs; }
 
   private void appendLogs(Consumer<StringBuilder> c) {
     StringBuilder sb = new StringBuilder(this.textLog.getText().trim());
     sb.append('\n');
     c.accept(sb);
     this.textLog.setText(sb.toString());
-    this.numMessagesSeen = this.src.size();
 
     // ensure as vertical scrollbars become necessary, we are *utilizing* them
     // https://stackoverflow.com/a/5150437; ie: behave like tail(1)
@@ -51,6 +49,7 @@ class ChatLogScrollPane extends JScrollPane implements Runnable {
       this.src.getPastIndex(this.numMessagesSeen, (Message m) -> {
         sb.append(ChatLogScrollPane.toHistoryLine(m));
       });
+      this.numMessagesSeen = this.src.size();
     });
   }
 

hw06/src/ChatterJFrame.java
@@ -190,7 +190,7 @@ public class ChatterJFrame extends JFrame implements ActionListener {
   }
 
   private void launchCachedChat(boolean isRecvd, final Remote unchecked) {
-    final String launchID = unchecked.toString();
+    final String launchID = unchecked.toTcpIpAppID();
     if (!this.chats.containsKey(launchID)) {
       this.chats.put(launchID, this.launchChat(isRecvd, unchecked));
       return;

hw06/src/History.java
@@ -76,10 +76,10 @@ public class History implements Runnable {
     this.broadcastHandler.accept(from, message);
   }
 
-  private void notifyListenersUnsafe(final String remoteID) {
-    this.log.debugf(
-        "notifying listeners on activity for '%s' [have: listeners=%s, default=%s]\n",
-        remoteID, this.registry.containsKey(remoteID), this.defaultListener != null);
+  private void notifyListenersUnsafe(final String remoteID, final String logTag) {
+    this.log.printf(
+        "notifying listeners of %s for '%s' [have: listeners=%s, default=%s]\n",
+        logTag, remoteID, this.registry.containsKey(remoteID), this.defaultListener != null);
     if (this.registry.containsKey(remoteID)) {
       this.registry.get(remoteID).forEach((Runnable listener) -> listener.run());
       return;
@@ -138,7 +138,7 @@ public class History implements Runnable {
             toSend.getMessage());
       });
 
-      RunLocked.safeRun(this.registryLock, () -> this.notifyListenersUnsafe(remoteID));
+      RunLocked.safeRun(this.registryLock, () -> this.notifyListenersUnsafe(remoteID, "SENDS"));
     });
   }
 
@@ -158,7 +158,7 @@ public class History implements Runnable {
             received.getMessage());
       });
 
-      RunLocked.safeRun(this.registryLock, () -> this.notifyListenersUnsafe(remoteID));
+      RunLocked.safeRun(this.registryLock, () -> this.notifyListenersUnsafe(remoteID, "RECVS"));
     });
   }
 

hw06/src/MessagingJFrame.java
@@ -19,19 +19,17 @@ public class MessagingJFrame extends JFrame implements ActionListener {
 
   private History hist;
   private final Remote remote;
+  private final String cause;
 
-  public MessagingJFrame(final String readable, History hist, final Remote r, UsernameService unc) {
-    super(String.format("chat [%s] with %s", readable, r.toString()));
+  public MessagingJFrame(final String cause, History hist, final Remote r, UsernameService unc) {
+    super(MessagingJFrame.buildTitle(r, cause));
+    this.cause = cause;
     this.hist = hist;
     this.remote = r;
     this.setLayout(new BorderLayout());
     this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-    this.chatLog = new ChatLogScrollPane(
-        20 /*rows*/,
-        DEFAULT_COLUMN_WIDTH /*cols*/,
-        this.hist.getHistoryWith(this.remote) /*warning: blocking*/);
-    this.hist.registerRemoteListener(this.remote, chatLog);
+    this.chatLog = new ChatLogScrollPane(20 /*rows*/, DEFAULT_COLUMN_WIDTH /*cols*/);
     this.getContentPane().add(chatLog, BorderLayout.CENTER);
 
     JPanel composePanel = new JPanel();
@@ -61,20 +59,34 @@ public class MessagingJFrame extends JFrame implements ActionListener {
     }).start();
   }
 
+  private static String buildTitle(final Remote r, final String cause) {
+    return String.format("chat [%s] with %s", cause, r.toString());
+  }
+
   private void metaLog(String msg) { this.composeField.setText("[loading] " + msg); }
 
   private void handleResolvedRemote() {
     if (this.remote.isValid()) {
+      this.setTitle(MessagingJFrame.buildTitle(this.remote, this.cause));
+      this.chatLog.metaLog(String.format("successfully identified peer as '%s'", this.remote.toString()));
       this.composeField.setText("");
+      this.chatLog.syncFrom(this.hist.getHistoryWith(this.remote) /*warning: blocking*/);
     } else {
+      this.setTitle(String.format("failed session: could not identify '%s'", this.remote.toString()));
       this.chatLog.metaLog(this.remote.error().getCause().toString());
       this.metaLog("failed to load chat");
       this.log.errorf(this.remote.error(), "failed to load chat with %s", this.remote);
       this.remote.error().printStackTrace(System.err);
     }
+
     this.composeField.setEnabled(this.remote.isValid());
     this.sendBtn.setEnabled(this.remote.isValid());
-    this.composeField.requestFocus();
+
+    if (this.remote.isValid()) {
+      this.composeField.requestFocus();
+      this.chatLog.run(); // process chat logs
+      this.hist.registerRemoteListener(this.remote, this.chatLog); // render logs as they come
+    }
   }
 
   public void actionPerformed(ActionEvent e) {

hw06/src/Remote.java
@@ -64,18 +64,14 @@ public class Remote {
   public int getPort() { return this.port; }
 
   public final String toString() {
-    return String.format(
-        "'%s':[is%s username service]",
-        this.toTcpIpAppID(),
-        this.isViaNameProtocol() ? "" : " not");
+    final String maybeUser = this.isViaNameProtocol() ? this.rawDest + "@" : "";
+    return String.format("%s%s", maybeUser, this.toTcpIpAppID());
   }
 
   public final String toTcpIpAppID() {
-    final String maybeUser = this.isViaNameProtocol() ? this.rawDest : "";
     final String maybeIP = this.hostIP();
     return String.format(
-        "[%s]:%s:%s",
-        maybeUser,
+        "%s:%s",
         maybeIP.length() == 0 && !this.isViaNameProtocol() ? this.rawDest : maybeIP,
         this.rawPort);
   }